460e3ebbffc3f03ceeaa3b498f098762047f7b20
Tomoo Suzuki <solosoprani2002@gmail.com>
2020-05-14 12:14:46 +0900

    refactor db datas

--- docker-entrypoint-initdb.d/02_user.sql
+++ docker-entrypoint-initdb.d/02_user.sql
@@ -5,13 +5,3 @@ create table users (
     user_type integer,
     date_reception varchar
 );
-
-insert into
-    users (
-        id_user,
-        password,
-        user_type,
-        date_reception
-    )
-values
-    ('00001', 'kxb1139', 3, '2019-11-12');
---
+++ docker-entrypoint-initdb.d/08_insert_user.sql
@@ -0,0 +1,9 @@
+insert into
+    users (
+        id_user,
+        password,
+        user_type,
+        date_reception
+    )
+values
+    ('00001', 'kxb1139', 3, '2019-11-12');
